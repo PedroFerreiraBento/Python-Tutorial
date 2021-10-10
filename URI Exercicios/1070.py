5e1fd3222a5f0b22c092c69b607c8dc82285b89a
PEDRO BENTO <pedrobento.informatica@gmail.com>
2021-10-10 17:11:46 -0300

    1070

--- URI Exercicios/1070.py
+++ URI Exercicios/1070.py
@@ -1,31 +1,4 @@
-
-# Metodo 3
-# X = list(X+1, X+(6*2), 2) if (X % 2 == 0) else list(X, X+(6*2), 2)
-# X = "\n".join(X)
-# print(X)
-
-# Metodo 2
-# if X % 2 == 0:
-#     X = list(range(X+1, X+(6*2), 2)) # [9, 11, 13, 15, 17]
-# for i in X:
-#     print(i)
-
-
-# Metodo 1
-# if X % 2 == 0:
-#     X+=1
-#     print(X)
-# for i in range(0,5):
-#     X+=2
-#     print(X)
-# -----------------------------------------------------
-
-X = int(input()) # 8
-if X % 2 == 0: # Par
-    X += 1
+X = int(input()) 
+X = list(X + 1, X + (6 * 2), 2) if (X % 2 == 0) else list(X, X + (6 * 2), 2)
+X = "\n".join(X)
 print(X)
-i = 1
-while i <=5:
-    X += 2
-    print(X)
-    i +=1
